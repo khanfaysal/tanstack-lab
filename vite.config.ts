4c6f32bc24fb8541acc3640345b1aa2b4f44518f
Faysal khan <mdfaysalkhancse@gmail.com>
2025-11-03 14:53:55 +0600

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 // vite.config.ts
-import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    // react's vite plugin must come after start's vite plugin
-    viteReact(),
-  ],
+  plugins: [tsConfigPaths(), tanstackStart(), viteReact()],
+  build: {
+    outDir: "dist",
+  },
 });
